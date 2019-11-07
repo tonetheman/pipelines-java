b73af31ecfca434536fee37fc240bbe785ac7ed4
Tony Colston <tonetheman@gmail.com>
2019-11-07 09:23:58 -0600

    added a println

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -3,6 +3,7 @@ package com.microsoft.demo;
 public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
+            System.out.println("hey tony");
             System.out.println("I am covered");
             return;
         }
